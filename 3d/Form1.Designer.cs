a2cb80f593af9d44d87a1da14a51f76f4a246810
Никита Скулыбердин <n.skul2012@mail.ru>
2021-03-29 21:13:36 +0300

    Добавьте файлы проекта.

---
+++ 3d/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace _3d
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1260, 675);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1164, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(1062, 16);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(96, 34);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1260, 675);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Montserrat", 16F);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Программирование трехмерной графики";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ 3d/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _3d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw(int w, int h, Graphics g, Pen pen)
+        {
+            g.Clear(Color.White);
+
+            int iterations = (int)numericUpDown1.Value;
+            int size = 2;
+
+            Point[] points =
+            {
+                new Point(w/2, h/2),
+                new Point(w/2, h/2 + size)
+            };
+
+            for (int i = 0; i < iterations; i++)
+            {
+                g.DrawLines(pen, points = Dragon(i, points));
+            }
+            
+        }
+
+        private Point[] Dragon(int iter, Point[] points)
+        {
+            List<Point> pt = new List<Point>();
+            double radian = (90 / 180d) * Math.PI;
+
+            int dx = points[points.Length - 1].X, 
+                dy = points[points.Length - 1].Y;
+                        
+            for (int i = 0; i < points.Length; i++)
+            {
+                pt.Add(points[i]);
+            }
+
+            for (int i = points.Length-1; i > 0; i--)
+            {
+                pt.Add(new Point((int)(dx + Math.Cos(radian) * (points[i-1].X - dx) - Math.Sin(radian) * (points[i-1].Y - dy)),
+                                (int)(dy + Math.Sin(radian) * (points[i-1].X - dx) + Math.Cos(radian) * (points[i-1].Y - dy))));
+            }
+
+            Point[] newPoints = new Point[pt.Count];
+
+            for (int i = 0; i < pt.Count; i++)
+                newPoints[i] = pt[i];
+
+            return newPoints;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen myPen = new Pen(Color.Black, 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            
+            Draw(pictureBox1.Width, pictureBox1.Height, g, myPen);
+        }
+    }
+}
